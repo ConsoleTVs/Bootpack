4faa36b66a954e37b1a8b4b1b019f152e71e7854
Erik Campobadal <ConsoleTVs@gmail.com>
2017-08-24 16:21:21 +0200

    Update web.php

--- src/Source/Routes/web.php
+++ src/Source/Routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::namespace('{{ NAMESPACE }}')->as('{{ NAME }}::')->middleware('web')->group(function () {
+Route::namespace('{{ NAMESPACE }}\Controllers')->as('{{ NAME }}::')->middleware('web')->group(function () {
     // Routes defined here have the web middleware applied
     // like the web.php file in a laravel project
     // They also have an applied controller namespace and a route names.
