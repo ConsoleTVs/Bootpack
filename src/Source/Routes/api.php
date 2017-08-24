61ffc46f5aa61156899ed93fe746cbbc4cd6219d
Erik Campobadal <ConsoleTVs@gmail.com>
2017-08-24 16:21:37 +0200

    Update api.php

--- src/Source/Routes/api.php
+++ src/Source/Routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::namespace('{{ NAMESPACE }}')->as('{{ NAME }}::')->middleware('api')->group(function () {
+Route::namespace('{{ NAMESPACE }}\Controllers')->as('{{ NAME }}::')->middleware('api')->group(function () {
     // Routes defined here have the api middleware applied
     // like the api.php file in a laravel project
     // They also have an applied controller namespace and a route names.
